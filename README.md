92631ac9c3a839abac03595783f8ba80e3e05c3d
KimHyeshin <hyeshin2530@gmail.com>
2019-03-24 19:57:21 +0900

    starter 변경 Nuxt+vuetify

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # fe-coursetube
 
-> Nuxt.js project
+> Nuxt.js + Vuetify.js project
 
 Front-end CourseTube는 Youtube에 있는 Front-end 관련 Video 중 좋은 컨텐츠를 선별(pick)하여 주제별로 Learning Course를 제공하는 사이트 입니다.
 
 ## More project details
 FE-CourseTube 프로젝트는 토이 프로젝트로 주말 혹은 평일에 시간을 내어 진행하고 있으며, 해당 프로젝트의 진행 과정 및 UI 설계 내용은 [여기](https://github.com/KimHyeshin/TIL/tree/master/toy-project/FE-CourseTube#fe-coursetube-front-end-coursetube)에 작성하고 있습니다.
+
 
 ## Build Setup
 
@@ -24,3 +25,4 @@ $ npm start
 $ npm run generate
 ```
 
+For detailed explanation on how things work, check out the [Nuxt.js](https://github.com/nuxt/nuxt.js) and [Vuetify.js](https://vuetifyjs.com/) documentation.
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,15 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
+  plugins: ['~/plugins/vuetify.js'],
+  css: ['~/assets/style/app.styl'],
   /*
   ** Customize the progress bar color
   */
@@ -21,11 +24,10 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
+    extractCSS: true,
+    extend (config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
@@ -36,4 +38,3 @@ module.exports = {
     }
   }
 }
-
